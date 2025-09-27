c36efbec8c844f72b76115639fda2399c6ecda12
pemmer.ferenc <pemmer.ferenc@gmail.com>
2025-09-27 11:34:00 +0200

    masodik ora

---
+++ py02.py
@@ -0,0 +1,78 @@
+from curses.ascii import isdigit
+import random
+
+'''szam = input("Kérek egy számot:")
+if isdigit(szam):
+    szam = int(szam)
+    if szam > 0:
+        print("Pozitiv")
+    elif szam < 0:
+        print("Negativ")
+    else:
+        print("Nulla")
+else:
+    print("Nem szamot adtal meg!")
+
+jegy = int(input("Kerek egy erdemjegyet:"))
+while jegy == 1  or jegy > 5 or jegy < 1:
+    jegy = int(input("Kerek egy erdemjegyet:"))
+    if jegy == 10:
+        print("Csillagos otos!")
+        break
+else:
+    print("Jol teljesitett")
+print("Gratulalunk!")
+
+kocka = random.randint(1,6)
+while True:
+    tipp = int(input("Kerek egy tippet:"))
+    if tipp == kocka:
+        break
+print("Gratulalok, eltaltad!")
+
+lista = (1,2,3,4,5,6,7,8,9)
+for elem in lista:
+    print(elem)
+
+def fy():
+    pass
+
+for elem in range(1,6):
+    if elem == 3:
+        continue
+    print(elem, "Nem leszek rossz!")
+
+try:
+    print(10/0)
+except ZeroDivisionError:
+    print("Hiba: Nullaval osztas")
+except NameError:
+    print("Hiba: Nevhiba")
+
+print("OK")
+
+while True:
+    try:
+        szam = int(input("Kerek egy egesz szamot:"))
+    except:
+        print("Nem egesz szamot adtal meg!")
+    else:
+        break'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ py022.py
@@ -0,0 +1,29 @@
+import turtle
+import random
+
+'''def teglalap(x, y):
+    # a = 5
+    # b = 4
+    ker = 2 * x + 2 * y
+    ter = x * y
+    return ker, ter
+
+def negyszog(x, y):
+    # a = 5
+    # b = 4
+    ker = 2 * x + 2 * y
+    ter = x * y
+    if x == y:
+        alakzat = "negyzet"
+    else:
+        alakzat = "teglalap"
+    return ker, ter, alakzat
+
+if __name__ == '__main__':
+    a = 2
+    b = 2
+    eredmeny = negyszog(a, b)
+    print(f"A {eredmeny[2]} kerulete: ", eredmeny[0])
+    print(f"A {eredmeny[2]} terulete: ", eredmeny[1])
+    print("Sajat hivas")'''
+
