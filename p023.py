c69db61466c51b44d64214183bb0e8ceeb2e34d2
pemmer.ferenc <pemmer.ferenc@gmail.com>
2025-09-27 12:28:56 +0200

    masodik ora

---
+++ p023.py
@@ -0,0 +1,34 @@
+import turtle
+import math
+
+def haromszog(szar):
+    turtle.hideturtle()
+    turtle.penup()
+    magassag = math.sqrt(szar**2 - (szar/2)**2)
+    turtle.goto(-szar/2, -magassag/2)
+    turtle.pendown()
+    turtle.color("red")        # piros szín
+    turtle.pensize(2)
+
+    # Rajzolás
+    turtle.forward(szar)
+    turtle.left(120)
+    turtle.forward(szar)
+    turtle.left(120)
+    turtle.forward(szar)
+    turtle.left(120)
+
+def main():
+    ablak = turtle.Screen()
+    ablak.setup(600, 600)
+    turtle.speed(0)
+
+    # Billentyűfigyelő események
+    turtle.listen()
+    turtle.onkey(lambda: haromszog(150), "h")
+    turtle.onkey(turtle.bye, "Escape")
+
+    ablak.mainloop()
+
+if __name__ == "__main__":
+    main()
--- py022.py
+++ py022.py
@@ -1,5 +1,4 @@
 import turtle
-import random
 
 '''def teglalap(x, y):
     # a = 5
@@ -26,4 +25,80 @@ if __name__ == '__main__':
     print(f"A {eredmeny[2]} kerulete: ", eredmeny[0])
     print(f"A {eredmeny[2]} terulete: ", eredmeny[1])
     print("Sajat hivas")'''
+
+
+#negyzet
+def negyzet():
+    turtle.hideturtle()
+    turtle.penup()
+    turtle.goto(-50, 50)
+    turtle.pendown()
+
+    for i in range(4):
+        turtle.forward(100)
+        turtle.right(90)
+
+
+
+#pontok rajzolasa
+def pont(x,y):
+    turtle.goto(x, y)
+    turtle.dot(10, "black")
+
+#dobas
+def dobas():
+    import random
+    turtle.hideturtle()
+    turtle.clear()
+    negyzet()
+    turtle.pensize(5)
+    szam = random.randint(1, 6)
+    turtle.penup()
+
+    if szam == 1:
+        pont(0, 0)
+    elif szam == 2:
+        pont(-30, 30)
+        pont(30, -30)
+    elif szam == 3:
+        pont(0,0)
+        pont(-30, 30)
+        pont(30, -30)
+    elif szam == 4:
+        pont(-30, 30)
+        pont(30, 30)
+        pont(-30, -30)
+        pont(30, -30)
+    elif szam == 5:
+        pont(0,0)
+        pont(-30, 30)
+        pont(30, 30)
+        pont(-30, -30)
+        pont(30, -30)
+    elif szam == 6:
+        pont(-30, 30)
+        pont(30, 30)
+        pont(-30, -30)
+        pont(30, -30)
+        pont(-30,0)
+        pont(30, 0)
+
+
+
+#App
+ablak = turtle.Screen()
+
+turtle.listen()
+turtle.onkey(dobas, "d")
+turtle.onkey(turtle.bye, "Escape")
+turtle.mainloop()
+
+
+
+
+
+
+
+
+
 
